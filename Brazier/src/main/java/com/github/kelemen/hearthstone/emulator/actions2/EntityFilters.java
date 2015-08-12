de78d9c4fdf36e0953289bbfbbcfb336084505d3
Attila Kelemen <attila.kelemen85@gmail.com>
2015-08-12 23:50:00 +0200

    Added keyword filters.

--- Brazier/src/main/java/com/github/kelemen/hearthstone/emulator/actions2/EntityFilters.java
+++ Brazier/src/main/java/com/github/kelemen/hearthstone/emulator/actions2/EntityFilters.java
@@ -1,9 +1,12 @@
 package com.github.kelemen.hearthstone.emulator.actions2;
 
+import com.github.kelemen.hearthstone.emulator.Keyword;
+import com.github.kelemen.hearthstone.emulator.LabeledEntity;
 import com.github.kelemen.hearthstone.emulator.World;
 import com.github.kelemen.hearthstone.emulator.actions.ActionUtils;
 import com.github.kelemen.hearthstone.emulator.parsing.NamedArg;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.jtrim.utils.ExceptionHelper;
@@ -38,6 +41,24 @@ public final class EntityFilters {
         return (World world, Stream<? extends Entity> entities) -> {
             List<Entity> elements = entities.collect(Collectors.<Entity>toList());
             return ActionUtils.pickMultipleRandom(world, count, elements).stream();
+        };
+    }
+
+    public static <Entity extends LabeledEntity> EntityFilter<Entity> withKeywords(
+            @NamedArg("keywords") Keyword... keywords) {
+        return withPredicate(ActionUtils.includedKeywordsFilter(keywords));
+    }
+
+    public static <Entity extends LabeledEntity> EntityFilter<Entity> withoutKeywords(
+            @NamedArg("keywords") Keyword... keywords) {
+        return withPredicate(ActionUtils.excludedKeywordsFilter(keywords));
+    }
+
+    public static <Entity> EntityFilter<Entity> withPredicate(Predicate<? super Entity> filter) {
+        ExceptionHelper.checkNotNullArgument(filter, "filter");
+
+        return (World world, Stream<? extends Entity> entities) -> {
+            return entities.filter(filter);
         };
     }
 
