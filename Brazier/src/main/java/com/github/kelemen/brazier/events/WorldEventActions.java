aed16777bc5bedcb631c41ac15ad88fee2a03db2
Attila Kelemen <attila.kelemen85@gmail.com>
2015-08-27 10:23:55 +0200

    Removed AttackEventActions.

--- Brazier/src/main/java/com/github/kelemen/brazier/events/WorldEventActions.java
+++ Brazier/src/main/java/com/github/kelemen/brazier/events/WorldEventActions.java
@@ -21,7 +21,10 @@ import com.github.kelemen.brazier.minions.MinionProvider;
 import com.github.kelemen.brazier.parsing.NamedArg;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import org.jtrim.utils.ExceptionHelper;
+
+import static com.github.kelemen.brazier.actions.BasicFilters.validMisdirectTarget;
 
 public final class WorldEventActions {
     public static final WorldEventAction<PlayerProperty, CardPlayEvent> PREVENT_CARD_PLAY = (world, self, eventSource) -> {
@@ -43,6 +46,20 @@ public final class WorldEventActions {
 
     public static final WorldEventAction<PlayerProperty, AttackRequest> MISS_TARGET_SOMETIMES
             = missTargetSometimes(1, 2);
+
+     public static final WorldEventAction<PlayerProperty, AttackRequest> MISSDIRECT = (world, self, eventSource) -> {
+         Predicate<TargetableCharacter> filter = validMisdirectTarget(eventSource);
+         List<TargetableCharacter> targets = new ArrayList<>();
+         ActionUtils.collectAliveTargets(world.getPlayer1(), targets, filter);
+         ActionUtils.collectAliveTargets(world.getPlayer2(), targets, filter);
+
+         TargetableCharacter selected = ActionUtils.pickRandom(world, targets);
+         if (selected == null) {
+             return UndoAction.DO_NOTHING;
+         }
+
+         return eventSource.replaceDefender(selected);
+     };
 
     public static WorldEventAction<PlayerProperty, AttackRequest> missTargetSometimes(
             @NamedArg("missCount") int missCount,
@@ -67,6 +84,24 @@ public final class WorldEventActions {
             }
 
             return eventSource.replaceDefender(newTarget);
+        };
+    }
+
+    public static WorldEventAction<PlayerProperty, AttackRequest> summonNewTargetForAttack(
+            @NamedArg("minion") MinionProvider minion) {
+        return (world, self, eventSource) -> {
+            Player targetPlayer = self.getOwner();
+            if (targetPlayer.getBoard().isFull()) {
+                return UndoAction.DO_NOTHING;
+            }
+
+            Minion summonedMinion = new Minion(targetPlayer, minion.getMinion());
+            UndoAction summonUndo = targetPlayer.summonMinion(summonedMinion);
+            UndoAction retargetUndo = eventSource.replaceDefender(summonedMinion);
+            return () -> {
+                retargetUndo.undo();
+                summonUndo.undo();
+            };
         };
     }
 
