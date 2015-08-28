7570d283413801ce817d610e717cc03add83e09e
Attila Kelemen <attila.kelemen85@gmail.com>
2015-08-28 14:02:10 +0200

    Generalized AuraAwareIntProperty.

--- Brazier/src/main/java/com/github/kelemen/brazier/abilities/AuraAwareIntProperty.java
+++ Brazier/src/main/java/com/github/kelemen/brazier/abilities/AuraAwareIntProperty.java
@@ -4,14 +4,12 @@ import com.github.kelemen.brazier.Priorities;
 import com.github.kelemen.brazier.Silencable;
 import com.github.kelemen.brazier.actions.UndoAction;
 import com.github.kelemen.brazier.events.UndoableUnregisterRef;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class AuraAwareIntProperty implements Silencable {
     private final int baseValue;
     private final int minValue;
 
-    private final List<BuffRef> buffRefs;
+    private final AuraAwarePropertyBase<IntPropertyBuff> impl;
 
     public AuraAwareIntProperty(int baseValue) {
         this(baseValue, Integer.MIN_VALUE);
@@ -21,19 +19,25 @@ public final class AuraAwareIntProperty implements Silencable {
         this.baseValue = baseValue;
         this.minValue = minValue;
 
-        this.buffRefs = new ArrayList<>();
+        this.impl = new AuraAwarePropertyBase<>((buffs) -> {
+            return (prev) -> {
+                int result = prev;
+                for (AuraAwarePropertyBase.BuffRef<IntPropertyBuff> buffRef: buffs) {
+                    result = buffRef.getBuff().buffProperty(result);
+                }
+                return result;
+            };
+        });
     }
 
     private AuraAwareIntProperty(AuraAwareIntProperty other) {
         this.baseValue = other.baseValue;
         this.minValue = other.minValue;
+        this.impl = other.impl.copy();
+    }
 
-        this.buffRefs = new ArrayList<>(other.buffRefs.size());
-        for (BuffRef buffRef: other.buffRefs) {
-            if (!buffRef.external) {
-                this.buffRefs.add(buffRef);
-            }
-        }
+    public AuraAwareIntProperty copy() {
+        return new AuraAwareIntProperty(this);
     }
 
     public UndoAction setValueTo(int newValue) {
@@ -73,97 +77,16 @@ public final class AuraAwareIntProperty implements Silencable {
     }
 
     public UndoableUnregisterRef addRemovableBuff(int priority, boolean external, IntPropertyBuff toAdd) {
-        int buffPos = findInsertPos(priority);
-
-        BuffRef buffRef = new BuffRef(priority, external, toAdd);
-        buffRefs.add(buffPos, buffRef);
-        return UndoableUnregisterRef.makeIdempotent(new UndoableUnregisterRef() {
-            @Override
-            public UndoAction unregister() {
-                for (int i = buffRefs.size() - 1; i >= 0; i--) {
-                    BuffRef candidate = buffRefs.get(i);
-                    if (candidate == buffRef) {
-                        int candidateIndex = i;
-                        buffRefs.remove(candidateIndex);
-                        return () -> buffRefs.add(candidateIndex, candidate);
-                    }
-                }
-                return UndoAction.DO_NOTHING;
-            }
-
-            @Override
-            public void undo() {
-                BuffRef removed = buffRefs.remove(buffPos);
-                if (removed != buffRef) {
-                    throw new IllegalStateException("Undo was called in an illegal state.");
-                }
-            }
-        });
-    }
-
-    private int findInsertPos(int priority) {
-        for (int i = buffRefs.size() - 1; i >= 0; i--) {
-            BuffRef buffRef = buffRefs.get(i);
-            if (buffRef.priority <= priority) {
-                return i + 1;
-            }
-        }
-        return 0;
-    }
-
-    public AuraAwareIntProperty copy() {
-        return new AuraAwareIntProperty(this);
-    }
-
-    private boolean hasNonExternalBuff() {
-        for (BuffRef buffRef: buffRefs) {
-            if (!buffRef.external) {
-                return true;
-            }
-        }
-        return false;
+        return impl.addRemovableBuff(priority, external, toAdd);
     }
 
     @Override
     public UndoAction silence() {
-        if (!hasNonExternalBuff()) {
-            return UndoAction.DO_NOTHING;
-        }
-
-        List<BuffRef> prevRefs = new ArrayList<>(buffRefs);
-        buffRefs.clear();
-        for (BuffRef buffRef: prevRefs) {
-            if (buffRef.external) {
-                buffRefs.add(buffRef);
-            }
-        }
-        return () -> {
-            buffRefs.clear();
-            buffRefs.addAll(prevRefs);
-        };
+        return impl.silence();
     }
 
     public int getValue() {
-        int result = baseValue;
-        for (BuffRef buffRef: buffRefs) {
-            result = buffRef.adjustValue(result);
-        }
-        return result;
-    }
-
-    private static final class BuffRef {
-        public final int priority;
-        public final boolean external;
-        private final IntPropertyBuff buff;
-
-        public BuffRef(int priority, boolean external, IntPropertyBuff buff) {
-            this.priority = priority;
-            this.external = external;
-            this.buff = buff;
-        }
-
-        public int adjustValue(int prevValue) {
-            return buff.buffProperty(prevValue);
-        }
+        int result = impl.getCombinedView().buffProperty(baseValue);
+        return result >= minValue ? result : minValue;
     }
 }
---
+++ Brazier/src/main/java/com/github/kelemen/brazier/abilities/AuraAwarePropertyBase.java
@@ -0,0 +1,135 @@
+package com.github.kelemen.brazier.abilities;
+
+import com.github.kelemen.brazier.Silencable;
+import com.github.kelemen.brazier.actions.UndoAction;
+import com.github.kelemen.brazier.events.UndoableUnregisterRef;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public final class AuraAwarePropertyBase<T> implements Silencable {
+    private final BuffDef<T> buffDef;
+    private final T combinedView;
+    private final List<BuffRef<T>> buffRefs;
+
+    public AuraAwarePropertyBase(BuffDef<T> buffDef) {
+        this.buffDef = buffDef;
+        this.buffRefs = new ArrayList<>();
+        this.combinedView = buffDef.viewCombinedBuffs(Collections.unmodifiableList(this.buffRefs));
+    }
+
+    private AuraAwarePropertyBase(AuraAwarePropertyBase<T> other) {
+        this.buffDef = other.buffDef;
+        this.buffRefs = new ArrayList<>(other.buffRefs.size());
+        this.combinedView = other.buffDef.viewCombinedBuffs(Collections.unmodifiableList(this.buffRefs));
+        for (BuffRef<T> buffRef: other.buffRefs) {
+            if (!buffRef.external) {
+                this.buffRefs.add(buffRef);
+            }
+        }
+    }
+
+    public UndoableUnregisterRef addRemovableBuff(int priority, boolean external, T toAdd) {
+        int buffPos = findInsertPos(priority);
+
+        BuffRef<T> buffRef = new BuffRef<>(priority, external, toAdd);
+        buffRefs.add(buffPos, buffRef);
+        return UndoableUnregisterRef.makeIdempotent(new UndoableUnregisterRef() {
+            @Override
+            public UndoAction unregister() {
+                for (int i = buffRefs.size() - 1; i >= 0; i--) {
+                    BuffRef<T> candidate = buffRefs.get(i);
+                    if (candidate == buffRef) {
+                        int candidateIndex = i;
+                        buffRefs.remove(candidateIndex);
+                        return () -> buffRefs.add(candidateIndex, candidate);
+                    }
+                }
+                return UndoAction.DO_NOTHING;
+            }
+
+            @Override
+            public void undo() {
+                BuffRef<?> removed = buffRefs.remove(buffPos);
+                if (removed != buffRef) {
+                    throw new IllegalStateException("Undo was called in an illegal state.");
+                }
+            }
+        });
+    }
+
+    private int findInsertPos(int priority) {
+        for (int i = buffRefs.size() - 1; i >= 0; i--) {
+            BuffRef<?> buffRef = buffRefs.get(i);
+            if (buffRef.priority <= priority) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    public AuraAwarePropertyBase<T> copy() {
+        return new AuraAwarePropertyBase<>(this);
+    }
+
+    private boolean hasNonExternalBuff() {
+        for (BuffRef<?> buffRef: buffRefs) {
+            if (!buffRef.external) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public UndoAction silence() {
+        if (!hasNonExternalBuff()) {
+            return UndoAction.DO_NOTHING;
+        }
+
+        List<BuffRef<T>> prevRefs = new ArrayList<>(buffRefs);
+        buffRefs.clear();
+        for (BuffRef<T> buffRef: prevRefs) {
+            if (buffRef.external) {
+                buffRefs.add(buffRef);
+            }
+        }
+        return () -> {
+            buffRefs.clear();
+            buffRefs.addAll(prevRefs);
+        };
+    }
+
+    public T getCombinedView() {
+        return combinedView;
+    }
+
+    public static interface BuffDef<T> {
+        public T viewCombinedBuffs(Collection<? extends BuffRef<T>> buffs);
+    }
+
+    public static final class BuffRef<T> {
+        private final int priority;
+        private final boolean external;
+        private final T buff;
+
+        public BuffRef(int priority, boolean external, T buff) {
+            this.priority = priority;
+            this.external = external;
+            this.buff = buff;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        public boolean isExternal() {
+            return external;
+        }
+
+        public T getBuff() {
+            return buff;
+        }
+    }
+}
